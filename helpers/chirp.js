7a79c2a73021c0c6fbaf99c459dd67dc817465df
Denis Ziegler <dzincolorado@yahoo.com>
2012-05-28 23:18:57 -0600

    cleaned up styles and layout

--- helpers/chirp.js
+++ helpers/chirp.js
@@ -1,6 +1,7 @@
 function chirp(tweet){
 	this.value = tweet;
 	this.createDate = new Date();
+	//this.createDate = this.createDate.toLocaleDateString() + this.createDate.toLocateTimeString();
 }
 
 exports.chirp = chirp
